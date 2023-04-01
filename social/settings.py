303d1ca72c1e917ddccde15779f7b2be3dcd7607
nikolasioannidis <nick.ioan10@gmail.com>
2023-04-01 12:31:32 +0300

    Dndating project pushed to Github

--- social/settings.py
+++ social/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kd1s83b5=c&!nxjupu(=u6-+ffrzfe-u@9n**!8^l!p&@*j$rb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
